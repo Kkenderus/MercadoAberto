a1afde2298ca432f5b8f7ba3f0a32cbc0fcfe1d1
Luan Romão <105988492+Kkenderus@users.noreply.github.com>
2022-07-02 13:03:08 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,8 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Mercado livre',
-      theme: ThemeData(
-      ),
+      theme: ThemeData(),
       home: const MyHomePage(title: 'Mercado Livre'),
     );
   }
@@ -38,25 +39,53 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-        backgroundColor: Color(0xfff5d415),
-        actions:<Widget> [
-            Icon( 
+        appBar: AppBar(
+          title: TextField (
+          decoration: InputDecoration(
+            fillColor: Colors.white,
+           enabledBorder:  OutlineInputBorder(
+              borderSide:  BorderSide(color: Colors.white, width: 90,),
+           ),
+          hintText: ('Pesquisar no Mercado Livre')),
+          ),
+          elevation: 0,
+          backgroundColor: Color(0xfff5d415),
+          actions: <Widget>[
+            Icon(
               Icons.shopping_cart_outlined,
               color: Colors.black,
               size: 26.0,
             ),
-        ],
-
+          ],
+          
           leading: Icon(
-          Icons.menu,
-          color: Colors.black,
-          size: 26.0,
+            Icons.menu,
+            color: Colors.black,
+            size: 26.0,
           ),
 
-          bottom: Text(''),
-        )
-      );
+          bottom: PreferredSize(
+            preferredSize: Size(70, 30),
+            child: ListTile(
+                leading: Icon(
+                  Icons.pin_drop_sharp,
+                  color: Colors.black,
+                  size: 26.0,
+                ),
+                title: Text('Enviar Para Valéria - Rua Jardim Paulista, 58')),
+          ),
+
+          ),
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment(0.0, 0.5),
+                colors: [
+                  Color.fromRGBO(245, 212, 21, 1), //cor amarela
+                  Color(0xfff5f5f5), //cor cinza
+                ]),
+          ),
+        ));
   }
 }
